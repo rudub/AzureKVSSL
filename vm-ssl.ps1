5cc5114d6401c648c462a3c0d3548e55122eecad
rudub <35490403+rudub@users.noreply.github.com>
2023-06-27 15:15:53 +0530

    Update vm-ssl.ps1

--- vm-ssl.ps1
+++ vm-ssl.ps1
@@ -4,25 +4,25 @@ $location =  “south india”
 $keyvaultName=“webserver"
 
 #Create new RG
-```New-AzureRmResourceGroup -ResourceGroupName $resourceGroup -Location $location```
+New-AzureRmResourceGroup -ResourceGroupName $resourceGroup -Location $location
 
 #Create new Keyvault
-```New-AzureRmKeyVault -VaultName $keyvaultName  -ResourceGroup $resourceGroup  -Location $location  -EnabledForDeployment```
+New-AzureRmKeyVault -VaultName $keyvaultName  -ResourceGroup $resourceGroup  -Location $location  -EnabledForDeployment
 
 #Define certificate policy
-```$policy = New-AzureKeyVaultCertificatePolicy  -SubjectName "CN=www.contoso.com"  -SecretContentType "application/x-pkcs12"  -IssuerName Self  -ValidityInMonths 12``` 
+$policy = New-AzureKeyVaultCertificatePolicy  -SubjectName "CN=www.contoso.com"  -SecretContentType "application/x-pkcs12"  -IssuerName Self  -ValidityInMonths 12
 
 #add certificate to azure key vault
-```Add-AzureKeyVaultCertificate -VaultName webserver  -Name webserver-cert -CertificatePolicy $policy```
+Add-AzureKeyVaultCertificate -VaultName webserver  -Name webserver-cert -CertificatePolicy $policy
 
 #Adding certificate to VM
 
-```$certURL=(Get-AzureKeyVaultSecret -VaultName $keyvaultName -Name  “webserver-cert").id```
+$certURL=(Get-AzureKeyVaultSecret -VaultName $keyvaultName -Name  “webserver-cert").id
 
-```$vm=Get-AzureRmVM -ResourceGroupName $resourceGroup -Name  “iis1"```
+$vm=Get-AzureRmVM -ResourceGroupName $resourceGroup -Name  “iis1"
 
-```$vaultId=(Get-AzureRmKeyVault -ResourceGroupName $resourceGroup -VaultName $keyVaultName).ResourceId ```
+$vaultId=(Get-AzureRmKeyVault -ResourceGroupName $resourceGroup -VaultName $keyVaultName).ResourceId 
 
-```$vm = Add-AzureRmVMSecret -VM $vm -SourceVaultId $vaultId -CertificateStore "My" -CertificateUrl $certURL ```
+$vm = Add-AzureRmVMSecret -VM $vm -SourceVaultId $vaultId -CertificateStore "My" -CertificateUrl $certURL
 
-`Update-AzureRmVM -ResourceGroupName $resourceGroup -VM $vm`
+Update-AzureRmVM -ResourceGroupName $resourceGroup -VM $vm
